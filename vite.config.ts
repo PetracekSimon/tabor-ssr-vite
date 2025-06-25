fc58d946e03c3b17b3b666c377512d476dda76e3
ŠImon Petráček <simpetracek@gmail.com>
2025-06-25 19:16:45 +0200

    log: mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
-  build: {
-    minify: true,
-    sourcemap: true
-  },
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+
+  return {
+    plugins: [react()],
+    server: { port: 3000 },
+    build: {
+      minify: true,
+      sourcemap: true
+    },
+  }
 });
