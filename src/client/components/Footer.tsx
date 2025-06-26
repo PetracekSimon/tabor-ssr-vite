cab804c6df71f6f9a17600eae18fe248a2894d81
Šimon Petráček <simonpetracekk@gmail.com>
2025-06-26 10:26:30 +0200

    deploy CI/CD fix

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       &copy; {new Date().getFullYear()} - Stanový tábor Kamenná
       <div className="flex justify-center mt-4">
         <a href="https://www.facebook.com/people/D%C4%9Btsk%C3%BD-stanov%C3%BD-t%C3%A1bor-Kamenn%C3%A1/61559911625763/" target="_blank" rel="noreferrer">
-          <img width={40} src="/assets/fb-logo.svg" alt="" />
+          <img width={40} height={40} src="/assets/fb-logo.svg" alt="" />
         </a>
       </div>
     </footer>
