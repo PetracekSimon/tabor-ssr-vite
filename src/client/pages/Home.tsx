34720d2e01ea5fee666dfd2b7322acf9897cf691
El3141 <lp@posli.to>
2025-07-01 11:16:26 +0200

    casovac pryc

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -27,10 +27,12 @@ const Home = () => {
             Vaše faraonova družina
           </div>
 
+          {/*
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center mt-8 text-center md:text-left">
             <h2 className="text-2xl font-bold">Do tábora zbývá už jen!</h2>
             <h1 className="text-4xl font-bold text-center"><Countdown targetDate={new Date("2025-06-30T09:00:00")} /></h1>
           </div>
+          */}
 
           <ImageGallery folder="2025" />
 
