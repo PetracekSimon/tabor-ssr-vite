3e0b1ce71b8dd2d8e4b5c10fd271eebc1d65cf8c
Šimon Petráček <simonpetracekk@gmail.com>
2025-08-19 08:33:13 +0200

    aria-label

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer className="mt-auto w-full py-4 text-center bg-gray-200 text-sm text-gray-700">
       &copy; 2025 - Stanový tábor Kamenná
       <div className="flex justify-center mt-4">
-        <a href="https://www.facebook.com/people/D%C4%9Btsk%C3%BD-stanov%C3%BD-t%C3%A1bor-Kamenn%C3%A1/61559911625763/" target="_blank" rel="noreferrer">
+        <a href="https://www.facebook.com/people/D%C4%9Btsk%C3%BD-stanov%C3%BD-t%C3%A1bor-Kamenn%C3%A1/61559911625763/" aria-label="Facebook - Dětský stanový tábor Kamenná" target="_blank" rel="noreferrer">
           <img width={40} height={40} src="/assets/fb-logo.svg" alt="" />
         </a>
       </div>
