04e9ac2889e23ec5d07efb2d64f8fc981d2b36a6
Šimon Petráček <simonpetracekk@gmail.com>
2025-09-15 21:55:38 +0200

    puppeteer fix?

--- src/server/application/controller.ts
+++ src/server/application/controller.ts
@@ -61,7 +61,19 @@ export async function generatePdf(application: Application) {
     </html>
   `;
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+  });
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
 
